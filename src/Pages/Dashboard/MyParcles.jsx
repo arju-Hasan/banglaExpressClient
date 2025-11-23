bd2512136ae732b817cf14df5a8f1e809934fddb
Arju-Hasan <rajuahmeed698@gmail.com>
2025-11-24 04:50:24 +0600

    Explore tanstack query and Load data using tanstack query

--- src/Pages/Dashboard/MyParcles.jsx
+++ src/Pages/Dashboard/MyParcles.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import UseAuth from '../../Hooks/UseAuth';
+import UseAxiosSecure from '../../Hooks/UseAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const MyParcles = () => {
+    const {user} = UseAuth();
+    const axiosSecure = UseAxiosSecure();
+
+    const { data: parcles = [] } = useQuery({
+        queryKey: ['myparcle', user?.email],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/parcles?email=${user.email}`)
+            return res.data;
+        }
+        
+    });
+
+
     return (
-        <div>
-            this is my parcle 
+        <div className='p-4'>
+            this is my parcle {parcles.length} 
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,20 @@ import { RouterProvider } from "react-router/dom";
 import { router } from "./Router/Router.jsx";
 import "./index.css";
 import AuthProvider from "./Contexts/AuthContext/AuthProvider.jsx";
- import { ToastContainer} from 'react-toastify';
+import { ToastContainer} from 'react-toastify';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
 const root = document.getElementById("root");
+const queryClient = new QueryClient()
+
 
 ReactDOM.createRoot(root).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
       <AuthProvider>
           <RouterProvider router={router} />,
             <ToastContainer />
       </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
