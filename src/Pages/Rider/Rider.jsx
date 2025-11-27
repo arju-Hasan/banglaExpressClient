611c7a98da74b7f7e38aa4a71cc281e88dfe7714
Arju-Hasan <rajuahmeed698@gmail.com>
2025-11-27 17:57:26 +0600

    Save Rider data in the database

--- src/Pages/Rider/Rider.jsx
+++ src/Pages/Rider/Rider.jsx
@@ -5,6 +5,7 @@ import { useLoaderData, useNavigate } from 'react-router';
 import UseAuth from '../../Hooks/UseAuth';
 import Container from '../../Components/Container';
 import rider from '../../assets/agent-pending.png'
+import Swal from 'sweetalert2';
 
 const Rider = () => {
      const {register, 
@@ -19,7 +20,7 @@ const Rider = () => {
     const serviceCentars = useLoaderData();     
     const regionsDup = serviceCentars.map(c => c.region);
     const regions = [...new Set(regionsDup)];
-    const senderRegions = useWatch({control, name:'senderRegions'});
+    const Regions = useWatch({control, name:'Regions'});
 
     const districtByRegions = region => {
         const regionDistricts = serviceCentars.filter(c => c.region === region );
@@ -28,6 +29,18 @@ const Rider = () => {
     }
     const handelBeARider = data =>{
             console.log(data);
+            axiosSecure.post('/riders', data)
+            .then( res => {
+                if(res.data.insertedId){
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Your application success. We Will rech to you 15 days",
+                        showConfirmButton: false,
+                        timer: 2500
+                        });
+                }
+            })
     }        
     
     return (
@@ -45,74 +58,59 @@ const Rider = () => {
                     {/* Rider info */}
                     <fieldset className="fieldset ">                        
                             {/* name */}
-                        <label className="label font-bold mt-2">Sender Name</label>
-                        <input type="text" {...register('SenderName')} defaultValue={user?.displayName} className="input w-full" placeholder="Sender Name" /> 
+                        <label className="label font-bold mt-2">Name</label>
+                        <input type="text" {...register('Name')} defaultValue={user?.displayName} className="input w-full" placeholder=" Name" /> 
                             {/* email */}
-                        <label className="label font-bold mt-2">Sender Email</label>
-                        <input type="email" {...register('SenderEmail')} defaultValue={user?.email} className="input w-full" placeholder="Sender email" />  
+                        <label className="label font-bold mt-2">Email</label>
+                          <input type="text" {...register('email')} defaultValue={user?.email} className="input w-full" placeholder=" Email" /> 
                          {/* sender phone number */}
-                        <label className="label font-bold mt-2">Sender Phone No</label>
-                        <input type="number" {...register('SenderPhoneNo')} className="input w-full" placeholder="Sender Phone" />
+                        <label className="label font-bold mt-2">Phone No</label>
+                        <input type="number" {...register('PhoneNo')} className="input w-full" placeholder="Phone No" />
 
-                        {/* sender Regions */}
+                        {/*  Regions */}
                         <fieldset className="fieldset">
-                        {/* <legend className="fieldset-legend">Sender Regions</legend> */}
-                        <label className="label font-bold mt-2">Sender Regions</label>
-                        <select defaultValue="Pick a Regions" {...register('senderRegions')} className="select w-full">
+                        <label className="label font-bold mt-2">Regions</label>
+                        <select defaultValue="Pick a Regions" {...register('Regions')} className="select w-full">
                             <option disabled={true}>Pick a Regions</option>
                             {
                                 regions.map((r,i)=><option key={i} value={r}>{r}</option>)
                             }                                
                         </select>                      
                         </fieldset>
-                         {/* sender Districts */}
-                        <fieldset className="fieldset">
-                        {/* <legend className="fieldset-legend">Sender District</legend> */}
-                        <label className="label font-bold mt-2">Sender District</label>
-                        <select defaultValue="Pick a District" {...register('senderDistricts')} className="select w-full">
+                         {/*  Districts */}
+                        <fieldset className="fieldset">                       
+                        <label className="label font-bold mt-2">District</label>
+                        <select defaultValue="Pick a District" {...register('Districts')} className="select w-full">
                             <option disabled={true}>Pick a District</option>
                             {
-                               districtByRegions(senderRegions).map((r,i)=><option key={i} value={r}>{r}</option>)
+                               districtByRegions(Regions).map((r,i)=><option key={i} value={r}>{r}</option>)
                             }                                
                         </select>                      
-                        </fieldset>
-                        {/* address */}
-                        <label className="label font-bold mt-2">Address</label>
-                        <input type="text" {...register('senderAddress')} className="input w-full" placeholder="Address" />
-                            {/* instruction */}
-                        <label className="label font-bold mt-2">Pickup Instruction</label>
-                        <input type="text" {...register('PickupInstruction')} className="input w-full" placeholder="Pickup Instructiont" /> 
-                    </fieldset>
-                    {/* rechive info */}
-                    <fieldset className="fieldset ">
-                        {/* <h2 className="text-4xl font-bold">Receiver Details</h2>  */}
-                            {/* name  */}
-                        <label className="label font-bold mt-2">Receiver Name</label>
-                        <input type="text" {...register('ReceiverName')} className="input w-full" placeholder="Receiver Name" /> 
-                        {/* email */}
-                        <label className="label font-bold mt-2">Receiver Email</label>
-                        <input type="email" {...register('ReceiverEmail')} className="input w-full" placeholder="Receiver email" /> 
-                        {/* phone number */}
-                        <label className="label font-bold mt-2">Receiver Phone No</label>
-                        <input type="number" {...register('ReceiverPhoneNo')} className="input w-full" placeholder="Receiver Phone" />  
-                          {/* Rwceiver Regions */}
-                       
-                        
-                        {/* address */}
-                         <label className="label font-bold mt-2">Receiver Address</label>
-                        <input type="text" {...register('receiverAddress')} className="input w-full" placeholder="Address" /> 
-                        <label className="label font-bold mt-2">Delivery Instruction</label>
-                        <input type="text" {...register('DeliveryInstruction')} className="input w-full" placeholder="Delivery Instruction" /> 
-                    </fieldset>
+                        </fieldset>                      
+                    </fieldset> 
+                    <fieldset className="fieldset">
+                         {/* address */}
+                        <label className="label font-bold mt-2">address</label>
+                        <input type="text" {...register('Address')}  className="input w-full" placeholder=" Address" />
+                        {/* NID */}
+                        <label className="label font-bold mt-2">Nid number</label>
+                        <input type="number" {...register('NID')}  className="input w-full" placeholder=" NID number" />
+                        {/* bike number */}
+                        <label className="label font-bold mt-2">Bike NUmber</label>
+                        <input type="text" {...register('BikeNumber')}  className="input w-full" placeholder="Bike Number" />
+                        {/* licence number */}
+                        <label className="label font-bold mt-2">Licence Number</label>
+                        <input type="text" {...register('licenceNumber')}  className="input w-full" placeholder="licence number" />
+                    </fieldset>                        
                 </div>
                 <p className='m-4'>* PickUp Time 10am-7pm Approx.</p>
-                <input type="submit" className='btn btn-primary m-4 w-full'  value="SendParcle" />
+                <input type="submit" className='btn btn-primary m-4 w-full'  value="Submit" />
             </form>
 
             </div>
             </Container>
         </div>
-    );
+    )
 };
 
 export default Rider;
