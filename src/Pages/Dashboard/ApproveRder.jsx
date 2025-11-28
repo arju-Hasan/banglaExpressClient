5331b9c6c9f8b98d495ad67e8b2381e508ff4509
Arju-Hasan <rajuahmeed698@gmail.com>
2025-11-28 06:23:22 +0600

    Rider Approve Panding and user role update

--- src/Pages/Dashboard/ApproveRder.jsx
+++ src/Pages/Dashboard/ApproveRder.jsx
@@ -1,22 +1,56 @@
 import React from 'react';
 import UseAxiosSecure from '../../Hooks/UseAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
-import { ScanEye, SquarePen, Trash2 } from 'lucide-react';
+import { Trash2 } from 'lucide-react';
+import { MdPersonAdd } from "react-icons/md";
+import { IoPersonRemoveSharp } from "react-icons/io5";
+import Swal from 'sweetalert2';
 
 const ApproveRder = () => {
     const axiosSecure = UseAxiosSecure();
-    const { data: riders = []} = useQuery({
+    const { refetch, data: riders = []} = useQuery({
         queryKey: ['riders', 'panding'],
         queryFn: async() =>{
             const res = await axiosSecure.get('/riders');
             return(res.data)
         }
     });
-    console.log(riders);
-     
-    const handelApproveRider = {
-
+    const updateRiderStatus = (rider, status) => {
+        const updateInfo = {status: status, email: rider.email }
+        axiosSecure.patch(`/riders/${rider._id}`, updateInfo)
+        .then(res =>{
+            if(res.data.modifiedCount){
+                refetch();
+                Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: `Rider status ${status}`,
+                showConfirmButton: false,
+                timer: 2500
+                });
+            }
+        })
     }
+     
+    const handelApproveRider = rider => {
+      updateRiderStatus(rider, 'approved')        
+    }
+    const handelRejctionRider = rider => {
+        updateRiderStatus(rider, 'rejction')
+    }
+    const handelRiderDelete = id => {     
+         axiosSecure.delete(`/riders/${id}`)
+        .then(res =>{
+        console.log(res.data);
+        if(res.data.deletedCount){                    
+        refetch(); // refresh data this ui
+        Swal.fire({
+        title: "Deleted!",
+        text: "Rider has been deleted.",
+        icon: "success"
+        });
+        }
+    })} 
 
     return (
         <div>
@@ -47,22 +81,23 @@ const ApproveRder = () => {
                             <td>{r.email}</td>
                             <td>{r.Districts}</td>                           
                             <td>{r.status}</td>                           
+                           <td>
+                                {r.status === "panding" ? (
+                                    <span className="btn btn-sm btn-primary text-white px-4">Pending</span>
+                                ) : r.status === "rejction" ? (
+                                    <span className="btn btn-sm text-red-500">Rejection</span>
+                                ) : (
+                                    <button className="btn btn-secondary hover:btn-primary text-accent btn-sm">
+                                    BEX-Rider
+                                    </button>
+                                )}
+                                </td>                            
                             <td>
-                                {
-                                    r.status === "panding" ? 
-                                     <button onClick={()=> handelApproveRider(r._id)}  className='btn btn-secondary hover:btn-primary text-accent btn-sm' >Make A Rider</button>
-                                    :                                    
-                                   <span className='btn btn-sm btn-primary text-white'>Panding</span>
-                                }
-                                </td>
+                                <button onClick={()=> handelApproveRider(r)} className='btn btn-square hover:bg-primary'><MdPersonAdd className='text-3xl' /></button>
 
-                            
-                            <td>
-                                <button className='btn btn-square hover:bg-primary'><ScanEye /></button>
+                                <button onClick={()=> handelRejctionRider(r)} className='btn btn-square hover:bg-primary mx-2'> <IoPersonRemoveSharp className='text-2xl' /></button>
 
-                                <button className='btn btn-square hover:bg-primary mx-2'> <SquarePen /></button>
-
-                                <button  className='btn btn-square hover:bg-primary'><Trash2 /></button>
+                                <button onClick={()=> handelRiderDelete(r._id)} className='btn btn-square hover:bg-primary'><Trash2 /></button>
                             </td>
                         </tr>
                     ))
